2e7185f43a28553d18271f07400582d58c07d98e
Caylin James <caylin@lougeek.com>
2017-11-03 08:29:51 -0400

    Halfway through 'Adding Links'

--- app.js
+++ app.js
@@ -9,30 +9,22 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name)
-        res.render('index', { name });
-    else
-        res.redirect('/hello');
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is..."});
-});
-
-app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username});
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
